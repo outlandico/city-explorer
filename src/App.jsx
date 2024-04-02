3341107fcc1ad70aff09bdec9666474360714a96
Adam Jimenez <outlandico@gmail.com>
2024-04-02 16:19:33 -0700

    keeping locationinfo in backhand

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import CityForm from './CityForm.jsx';
 import './App.css'; // Import the CSS file
 
 function App() {
   const [city, setCity] = useState('');
-  const [location, setLocation] = useState({});
-  const [error, setError] = useState('');
+  const [error, setError] = useState(0);
 
   async function getLocation() {
     if (!city) {
       setError("Please enter a city!");
-      setLocation({});
       return;
     }
 
     try {
-      const response = await fetch(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${city}&format=json`);
+      const response = await fetch(`https://us1.locationiq.com/v1/search?key=${import.meta.env.REACT_APP_LOCATION_API_KEY}&q=${city}&format=json`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const jsonData = await response.json();
-      setLocation(jsonData);
+      setCity(jsonData)
       setError('');
     } catch (error) {
       console.error("Error getting location information", error);
--- src/LocationInfo.jsx
+++ src/LocationInfo.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 function LocationInfo({ location, accessToken }) {
   return (
     <>
